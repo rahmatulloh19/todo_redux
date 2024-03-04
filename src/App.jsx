5b7b3223488639dd666b941178078cdcde1e0f65
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-03-05 01:06:45 +0500

    created Todo page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Register } from "./pages/Register/Register";
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./pages/Login/Login";
+import { Todo } from "./pages/Todo/Todo";
+import { useDispatch } from "react-redux";
+import { setToken } from "./redux/token/tokenAction";
+import { setUser } from "./redux/user/userAction";
 
 function App() {
+  const token = localStorage.getItem("token");
+  const dispatch = useDispatch();
+
+  dispatch(setToken(localStorage.getItem("token")));
+  dispatch(setUser(JSON.parse(localStorage.getItem("me"))));
+
+  if (token) {
+    return <Todo />;
+  }
+
   return (
     <Routes>
       <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" index element={<Login />} />
     </Routes>
   );
 }
